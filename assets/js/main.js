75fcf23e53f43372aa08dfe6f68648d5e719b0b4
alhelal522 <alhelal522@gmail.com>
2025-05-14 12:00:33 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.slider-test').slick({
+    dots: true,             
+    arrows: false,            
+    autoplay: true,          
+    autoplaySpeed: 3000,     
+    infinite: true,          
+    speed: 600,              
+    slidesToShow: 1,         
+    slidesToScroll: 1 
+  });
+});
